a9f103f2aca4b7887360f2f0360edb373ce64162
lisa-naprimer <113773266+lisa-naprimer@users.noreply.github.com>
2022-10-28 20:08:51 +0300

    Add files via upload

---
+++ 6.11.py
@@ -0,0 +1,5 @@
+s = 'dddddaaaaaaadaaaa'
+s = s.replace('a','o')
+c = s.count('o')
+x = len(s)
+print(c,x)
---
+++ 7.4.1.py
@@ -0,0 +1,4 @@
+list1 = [1,2,3,4,5,6]
+m_n = max(list1)
+print(list1.index(max(list1))) 
+print(m_n)
